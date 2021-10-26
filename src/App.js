e72a81468f19ac02a09a44175b08986c65521999
ajaysurya8888 <ajaysurya.m98@gmail.com>
2021-10-26 07:47:15 +0530

    redux setup

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
+import {Provider} from "react-redux";
+import store from "./store";
 function App() {
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
       <Header></Header>
@@ -14,6 +17,7 @@ function App() {
      <Route exact path="/addProject" component={AddProject} />
     </div>
     </Router>
+    </Provider>
   );
   }
 export default App;
